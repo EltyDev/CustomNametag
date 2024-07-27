0119afd1d92b7917bc00ce2d684a4fe16762cdac
Venodez <30352734+EltyDev@users.noreply.github.com>
2024-07-27 13:30:10 +0200

    Add unversioned files

---
+++ src/main/java/fr/elty/customnametag/CustomNametagMod.java
@@ -0,0 +1,10 @@
+package fr.elty.customnametag;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = CustomNametagMod.MODID, version = CustomNametagMod.VERSION)
+public class CustomNametagMod {
+
+    public static final String MODID = "customnametag";
+    public static final String VERSION = "1.0";
+}
